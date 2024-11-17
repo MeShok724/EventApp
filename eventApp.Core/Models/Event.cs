6a09163dd3edd9cf8e6d54f54d827396fbe67270
Mikhail Kapytok <mishakopytok@gmail.com>
2024-11-18 00:40:24 +0300

    Add project files.

---
+++ eventApp.Core/Models/Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eventApp.Core.Models
+{
+    public class Event
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Location { get; set; }
+        public string Category { get; set; }
+        public int MaxParticipants { get; set; }
+        public ICollection<Participant> Participants { get; set; }
+        public byte[]? Image { get; set; }
+    }
+}

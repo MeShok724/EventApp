b7df6250a8f82f253907c18fc740d40e00a66289
Mikhail Kapytok <mishakopytok@gmail.com>
2024-11-22 01:02:23 +0300

    part of validation

--- eventApp.Core/Models/Event.cs
+++ eventApp.Core/Models/Event.cs
@@ -3,6 +3,10 @@ namespace EventApp.Core.Models
 {
     public class Event
     {
+        public const int MAX_NAME_LENGTH = 100;
+        public const int MAX_DESCRIPTION_LENGTH = 250;
+        public const int MAX_LOCATION_LENGTH = 250;
+        public const int MAX_CATEGORY_LENGTH = 100;
         public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
@@ -13,5 +17,35 @@ namespace EventApp.Core.Models
         public byte[] Image { get; set; } = [];
 
         public List<Participant> Participants { get; set; } = [];
+
+        public static (Event, string) Create(Guid Id, string Name, string Description, DateTime DateTime,
+            string Location, string Category, int MaxParticipants, byte[] Image)
+        {
+            string error = string.Empty;
+
+            if (string.IsNullOrEmpty(Name) || Name.Length > MAX_NAME_LENGTH) {
+                error = "Name can not be empty or longer than 100 symbols";
+            }
+            if (string.IsNullOrEmpty(Description) || Description.Length > MAX_DESCRIPTION_LENGTH)
+            {
+                error = "Description can not be empty or longer than 100 symbols";
+            }
+
+            Event Event = new Event(Id, Name, Description, DateTime, Location, Category, MaxParticipants, Image);
+            return (Event,  error);
+        }
+
+        private Event(Guid Id, string Name, string Description, DateTime DateTime, string Location,
+            string Category, int MaxParticipants, byte[] Image)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Description = Description;
+            this.DateTime = DateTime;
+            this.Location = Location;
+            this.Category = Category;
+            this.MaxParticipants = MaxParticipants;
+            this.Image = Image;
+        }
     }
 }

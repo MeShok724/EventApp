bed6bc141f96a1f4f0a12c13bb76571b4bf8c98a
Mikhail Kapytok <mishakopytok@gmail.com>
2024-12-06 03:13:50 +0300

    participant repository, created ParticipantEvent entity

--- eventApp.Postgres/EventAppDbContext.cs
+++ eventApp.Postgres/EventAppDbContext.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using EventApp.Core.Models;
+using eventApp.Postgres.Entites;
 
 namespace EventApp.Postgres
 {
     public class EventAppDbContext(DbContextOptions<EventAppDbContext> options) : DbContext(options)
     {
         public DbSet<EventEntity> Events { get; set; }
-        public DbSet<ParticipantRepository> Participants { get; set; }
+        public DbSet<ParticipantEntity> Participants { get; set; }
+        public DbSet<ParticipantEventEntity> ParticipantEvents { get; set; }
     }
 }
--- eventApp.Postgres/Repositories/ParticipantRepository.cs
+++ eventApp.Postgres/Repositories/ParticipantRepository.cs
@@ -1,4 +1,6 @@
-﻿using EventApp.Postgres;
+﻿using EventApp.Core.Models;
+using EventApp.Postgres;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,5 +12,48 @@ namespace eventApp.Postgres.Repositories
     public class ParticipantRepository(EventAppDbContext context)
     {
         private readonly EventAppDbContext _context = context;
+        public async Task<List<Participant>> GetAll()
+        {
+            var resp = await _context.Participants
+                .AsNoTracking()
+                .ToListAsync();
+            return resp
+                .Select(pE => Participant.Create(pE.Id, pE.FirstName, pE.LastName, pE.DateOfBirth, pE.Email).Item1)
+                .ToList();
+        }
+        public async Task<Participant?> GetById(Guid id)
+        {
+            var resp = await _context.Participants
+                .AsNoTracking()
+                .FirstOrDefaultAsync(p => p.Id == id);
+            if (resp == null)
+                return null;
+            return Participant.Create(resp.Id, resp.FirstName, resp.LastName, resp.DateOfBirth, resp.Email).Item1;
+        }
+        public async Task<Guid?> Add(Participant participant)
+        {
+            if (participant == null)
+                return null;
+            ParticipantEntity participantEntity = new ParticipantEntity
+            {
+                Id = participant.Id,
+                FirstName = participant.FirstName,
+                LastName = participant.LastName,
+                DateOfBirth = participant.DateOfBirth,
+                Email = participant.Email
+            };
+
+            var resp = await _context.Participants
+                .AddAsync(participantEntity);
+            await _context.SaveChangesAsync();
+            return participantEntity.Id;
+        }
+        public async Task<Guid> DeleteEvent(Guid participantId, Guid eventId)
+        {
+            await _context.ParticipantEvents
+                .Where(pE => pE.EventId == eventId && pE.ParticipantId == participantId)
+                .ExecuteDeleteAsync();
+            return participantId;
+        }
     }
 }
